792401d1de54b67b64291622280911c852fafd1f
silverlightning <cwwendyyang@gmail.com>
2014-07-27 16:03:13 -0700

    updated functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Takes or makes a matrix and allows for solved data (inverse of a 
+## matrix) to be retrieved if available, otherwise to be calculated
+## and cached.
 
-## Write a short comment describing this function
+
+## makeCacheMatrix takes data from given matrix or makes it into 
+## a matrix if none is given, and adds another field to allow
+## for the inverse to be cached
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## cachSolve will replace NULL with solved data if this field in 
+## makeCacheMatrix is empty, otherwise it will say "getting cached
+## data and provides the cached data
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
